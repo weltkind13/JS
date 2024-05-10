079597552721e5d39c0320c441ef6387dc28c5fe
Viktor <weltkind13@gmail.com>
2024-05-10 21:35:13 +0300

    last of problem 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
+// let value;
+// value = false;
+// console.log(typeof value);
+// const newValue = String(value);
+// let value = -10;
+// const newValue = Number(value);
+// if (value >= 10) {
+//   console.log(value - 5);
+// } else {
+//   console.log(value + 5);
+// }
 
+// let value = prompt("Enter number of 1 for 12");
+
+// if (value === "1") {
+//   console.log("Janvary");
+// } else if (value === "2") {
+//   console.log("febrary");
+// } else if (value === "3") {
+//   console.log("march");
+// } else if (value === "4") {
+//   console.log("april");
+// } else if (value === "5") {
+//   console.log("may");
+// } else if (value === "6") {
+//   console.log("june");
+// } else if (value === "7") {
+//   console.log("july");
+// } else if (value === "8") {
+//   console.log("august");
+// } else if (value === "9") {
+//   console.log("september");
+// } else if (value === "10") {
+//   console.log("october");
+// } else if (value === "11") {
+//   console.log("november");
+// } else if (value === "12") {
+//   console.log("december");
+// } else console.log("Retry");
+
+let value = prompt("Input 3chars number");
+
+(value = x), y, z;
+console.log(x + y + z);
